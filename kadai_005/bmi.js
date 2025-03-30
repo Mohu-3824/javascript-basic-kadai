0be334673cc29edcff506c676b08c335b2c55bbd
Mohu-3824 <maki.yoshimura.97@gmail.com>
2025-03-30 23:08:20 +0900

    kadai_005のアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// 体重（kg）*身長（m）の宣言・値の代入
+let weight = 68;
+let height = 1.7;
+
+// 身長*身長の値の代入
+let height_height = height * height;
+
+// BMIの値の代入
+let bmi = weight / height_height;
+
+// コンソールへの出力
+console.log(bmi);
